6139f2473aa5970a5bbd18f1d60e3593c0758011
thaontt2134 <thaontt2134@fpt.com>
2024-06-23 23:10:29 +0900

    commit fix server.js

--- server.js
+++ server.js
@@ -18,8 +18,20 @@ app.get("/filteredimage", async (req, res) => {
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
 
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
   // Validate the image_url query parameter
-  debugger;
   if (!image_url) {
     return res.status(400).send({ message: "image_url is required" });
   }
@@ -35,25 +47,18 @@ app.get("/filteredimage", async (req, res) => {
       }
 
       // Delete any files on the server on finish of the response
-      deleteLocalFiles([filteredPath]);
+      deleteLocalFiles([filteredPath])
+        .then(() => {
+          console.log("Files deleted successfully");
+        })
+        .catch((error) => {
+          console.error("Error deleting files:", error);
+        });
     });
   } catch (error) {
-    return res.status(422).send({ message: "Error processing the imagesss" });
+    return res.status(422).send({ message: "Image processing failed" });
   }
 });
-
-// IT SHOULD
-//    1
-//    1. validate the image_url query
-//    2. call filterImageFromURL(image_url) to filter the image
-//    3. send the resulting file in the response
-//    4. deletes any files on the server on finish of the response
-// QUERY PARAMATERS
-//    image_url: URL of a publicly accessible image
-// RETURNS
-//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-/**************************************************************************** */
 
 //! END @TODO1
 
