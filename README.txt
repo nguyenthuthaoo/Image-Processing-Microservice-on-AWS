d3c6712470f619602b2adba609c170cc40fbf62d
thaontt2134 <thaontt2134@fpt.com>
2024-06-23 22:28:04 +0900

    commit 2306 first

---
+++ README.txt
@@ -0,0 +1,2 @@
+http://projectstartercode-dev.us-east-1.elasticbeanstalk.com/
+https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg
---
+++ server.js
@@ -0,0 +1,70 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util.js";
+
+// Init the Express application
+const app = express();
+
+// Set the network port
+const port = process.env.PORT || 8082;
+
+// Use the body parser middleware for post requests
+app.use(bodyParser.json());
+
+app.get("/filteredimage", async (req, res) => {
+  const { image_url } = req.query;
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+
+  // Validate the image_url query parameter
+  debugger;
+  if (!image_url) {
+    return res.status(400).send({ message: "image_url is required" });
+  }
+
+  try {
+    // Call filterImageFromURL(image_url) to filter the image
+    const filteredPath = await filterImageFromURL(image_url);
+
+    // Send the resulting file in the response
+    res.sendFile(filteredPath, (err) => {
+      if (err) {
+        return res.status(500).send({ message: "Error processing the images" });
+      }
+
+      // Delete any files on the server on finish of the response
+      deleteLocalFiles([filteredPath]);
+    });
+  } catch (error) {
+    return res.status(422).send({ message: "Error processing the imagesss" });
+  }
+});
+
+// IT SHOULD
+//    1
+//    1. validate the image_url query
+//    2. call filterImageFromURL(image_url) to filter the image
+//    3. send the resulting file in the response
+//    4. deletes any files on the server on finish of the response
+// QUERY PARAMATERS
+//    image_url: URL of a publicly accessible image
+// RETURNS
+//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+/**************************************************************************** */
+
+//! END @TODO1
+
+// Root Endpoint
+// Displays a simple message to the user
+app.get("/", async (req, res) => {
+  res.send("try GET /filteredimage?image_url={{}}");
+});
+
+// Start the Server
+app.listen(port, () => {
+  console.log(`server running http://localhost:${port}`);
+  console.log(`press CTRL+C to stop server`);
+});
